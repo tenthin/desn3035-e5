f498d5968e3c7f7b2c695572fa5df7c596452a26
Tenzin Thinley <60735298+tenthin@users.noreply.github.com>
2024-10-15 17:18:53 +0000

    Puppeteer and Lighthouse

--- scripts/main.mjs
+++ scripts/main.mjs
@@ -1,3 +1,6 @@
+import fs from 'fs';
+import lighthouse from 'lighthouse';
+
 import puppeteer from 'puppeteer';
 // Or import puppeteer from 'puppeteer-core';
 
@@ -9,12 +12,21 @@ const page = await browser.newPage();
 await page.goto('https://tenthin.github.io/desn3035-e5/');
 
 // Set screen size.
-await page.setViewport({width: 1080, height: 1024});
+await page.setViewport({ width: 1080, height: 1024 });
 
 await page.waitForNetworkIdle();
 
 await page.screenshot({
     path: "auto_screenshot.png"
 })
+
+const options = { 
+    output: 'html' 
+};
+const runnerResult = await lighthouse('https://tenthin.github.io/desn3035-e5', options, undefined, page);
+
+// `.report` is the HTML report as a string
+const reportHtml = runnerResult.report;
+fs.writeFileSync('auto_report.html', reportHtml);
 
 await browser.close();
