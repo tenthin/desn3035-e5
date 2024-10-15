b03aecb025eccc8cfbb9a688f21557d1d62df56d
Tenzin Thinley <60735298+tenthin@users.noreply.github.com>
2024-10-15 17:45:20 +0000

    url correction

--- scripts/main.mjs
+++ scripts/main.mjs
@@ -9,7 +9,7 @@ const browser = await puppeteer.launch();
 const page = await browser.newPage();
 
 // Navigate the page to a URL.
-await page.goto('https://tenthin.github.io/desn3035-e5');
+await page.goto('https://tenthin.github.io/desn3035-e5/');
 
 // Set screen size.
 await page.setViewport({ width: 1080, height: 1024 });
@@ -23,7 +23,7 @@ await page.screenshot({
 const options = { 
     output: 'html' 
 };
-const runnerResult = await lighthouse('https://tenthin.github.io/desn3035-e5', options, undefined, page);
+const runnerResult = await lighthouse('https://tenthin.github.io/desn3035-e5/', options, undefined, page);
 
 // `.report` is the HTML report as a string
 const reportHtml = runnerResult.report;
